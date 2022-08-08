25c25359f5028a65e81e35284c5c48f03d5f8529
Ariman999 <85644065+Ariman999@users.noreply.github.com>
2022-08-08 14:10:24 +0300

    second_task
    Пользователь вводит время в секундах. Переведите время в часы, минуты, секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.

---
+++ second_task.py
@@ -0,0 +1,5 @@
+primus_seconds = int(input('Ввведите колличество секунд: '))
+hours = primus_seconds // 3600
+minutes = (primus_seconds % 3600) // 60
+seconds = primus_seconds % 60
+print(f'время: {hours}:{min27utes}:{seconds}')
