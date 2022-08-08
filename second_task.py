f7e4a141444ac90fdeecf2ef0fc53689c4ff12c5
Ariman999 <85644065+Ariman999@users.noreply.github.com>
2022-08-08 14:23:42 +0300

    second_task(change)

--- second_task.py
+++ second_task.py
@@ -2,4 +2,4 @@ primus_seconds = int(input('Ввведите колличество секунд
 hours = primus_seconds // 3600
 minutes = (primus_seconds % 3600) // 60
 seconds = primus_seconds % 60
-print(f'время: {hours}:{min27utes}:{seconds}')
+print(f'время: {hours}:{minutes}:{seconds}')
